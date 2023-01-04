dc2e82ff34d819f18e4c93dd10c2fcfb25864247
tiwavaldese <tiwaval7@gmail.com>
2023-01-04 14:33:25 +0100

    index.js

---
+++ index.js
@@ -0,0 +1,131 @@
+alert
+    //your javascript goes here!
+    console.log("Hello ,world")
+
+    let username='Mary';
+     let age=24;
+     let message= 'hello';
+
+     console.log(username)
+     console.log(age)
+     console.log(message)
+
+     let x = 30;
+     let y =50;
+     let z =x+y;
+     console.log(z)
+    
+     let xa = 60;
+     let yb =10;
+     let zc =xa-yb;
+     console.log(zc)
+    
+     let x1 = 30;
+     let y1 =50;
+     let z1 =x1*y1;
+     console.log(z1)
+
+     let x2 = 40;
+     let y2 =30;
+     let z2 =x2/y2;
+     console.log(z2)
+
+     let x3 = 5;
+     x3++;
+     let z3 =x3;
+     console.log(z3)
+
+     let x4 = 5;
+     x4--;
+     let z4 =x4;
+     console.log(z4)
+
+     let x5= 5;
+     let z5= x**2;
+     console.log(z5)
+
+     let xb = 237e5;
+     let zb  = 237e-5;
+     console.log(xb)
+     console.log(zb)
+
+     let x6 = 10;
+     let y6 = 5;
+     let z6 = 10+ 5;
+     console.log(z6)
+
+     let x7= 16 + 4 + "volvo"
+     console.log(x7)
+
+     let x8= "volvo" + 16 + 4;
+     console.log(x8)
+
+     let username1= "he is called 'Johny'"
+     console.log(username1)
+
+     let username2= 'he is called "Johny"'
+     console.log(username2)
+
+     let x9 = 5;
+     let y9 = 5;
+     let z9 = 6;
+     console.log(x9==y9)
+     console.log(x9==z9)
+
+     let lenght = 16;
+    const  firstName ="mercy";
+    const lastName1 ="Doe";
+    const myBirthday ='05/04/2022';
+   
+     console.log(lenght)
+     console.log(firstName)
+     console.log(lastName1)
+     console.log(myBirthday)
+     
+     let x11 = (0.2*10 + 0.1*10)/10;
+     console.log(x11)
+     
+     let x12 = "10";
+     let y12 = 20;
+     let z12 = x12 + y12;
+     console.log(z12)
+
+     let x13 = 10;
+     let y13 = 20;
+     let z13 = "30";
+     let result = x13 + y13 + z13
+     console.log(result)
+
+     let x14 = "100";
+     let y14 = "10";
+     let z14 = x14/y14;
+     console.log(z14)
+
+     let x15 = "100";
+     let y15 = "10";
+     let z15 = x15*y15;
+     console.log(z15)
+
+     let x16 = "100";
+     let y16 = "10";
+     let z16 = x16 - y16;
+     console.log(z16)
+
+     let x17 = "100";
+     let y17 = "10";
+     let z17 = x17 + y17;
+     console.log(z17)
+
+
+
+
+     const sales=100;
+     const cost= 50;
+     const profit=sales+cost;
+
+     console.log(profit);
+
+  
+
+
+
