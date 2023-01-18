e917a85a61eea9e444c8a0268b035a5836119d92
tiwavaldese <tiwaval7@gmail.com>
2023-01-18 18:39:55 +0100

    NOT.js

---
+++ NOT.js
@@ -0,0 +1,11 @@
+alert
+
+let w= true;
+console.log(!w);
+
+console.log(!true);
+console.log(!0);
+console.log(!!'non empty string')
+console.log(!!null);
+console.log(Boolean('non empty string'))
+console.log(Boolean(null))
