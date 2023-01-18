79933e51e61c6c559afa90e2e90d8207acdb4d15
tiwavaldese <tiwaval7@gmail.com>
2023-01-18 09:15:11 +0100

    whileLoop.js

---
+++ whileLoop.js
@@ -0,0 +1,23 @@
+alert
+
+
+while(i< 10){
+    console.log( "your number is" + i);
+    i++;
+    
+  }
+
+  let Age=8;
+  while(Age<10){
+    console.log("your Age is less than 10");
+    Age++;
+  }
+
+  
+  const caRs= ['BMW','Volvo','Saab','Ford'];
+  let m=0;
+  let teXt="";
+  while(caRs[m]){
+    teXt += caRs[m];
+    m++;
+  }console.log(m);
