90fac2c5d9b050a3446191647533ca38ef57763f
tiwavaldese <tiwaval7@gmail.com>
2023-01-18 09:11:39 +0100

    dowhileLoop.js

---
+++ dowhileLoop.js
@@ -0,0 +1,18 @@
+alert
+
+
+do{
+    document.write("The number is" +i);
+    i++;
+  }
+  while(i<10);
+  console.log('your number is' +i);
+
+  let X=7;
+  do{
+    document.write("<h2>Tanmay</h2>");
+    X--;
+  }while(X<5);
+  console.log(X)
+
+ 
