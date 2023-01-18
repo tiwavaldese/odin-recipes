cc4372782d6c6036cd7483cacd07bd077e1472d2
tiwavaldese <tiwaval7@gmail.com>
2023-01-18 18:32:35 +0100

    AND.js

---
+++ AND.js
@@ -0,0 +1,42 @@
+alert
+
+
+let j4=10,k4=10;
+if(j4 === 10 && k4 === 10){
+  console.log('true');
+}else{
+  console.log('false');
+}
+
+let j5=10,k5=10;
+if(j5=== 10 && k5 === 5){
+  console.log('true');
+}else{
+  console.log('false');
+}
+
+console.log(true&&true);
+console.log(false&&true);
+console.log(true&&false);
+console.log(false&&false);
+
+let hou= 12
+let minute= 30;
+if(hou == 12 && minute == 30){ 
+  console.log('The time is 12:30');
+}
+
+console.log(1 && 2 && null && 3);
+console.log(1 && 2 && 3);
+console.log(a && b || c && d);
+
+let n=1;
+if(x>0) console.log('greater than zero');
+
+console.log(1 && null && 2);
+
+console.log(null || 2 && 3 || 4);
+
+
+if (-1 && 0)console.log('second');
+if (null ||-1 && 1)console.log('third');
