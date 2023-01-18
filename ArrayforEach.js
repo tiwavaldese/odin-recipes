6c52e52aadb99164f9b048fb87b58973d02e0a0a
tiwavaldese <tiwaval7@gmail.com>
2023-01-18 09:10:40 +0100

    ArrayforEach.js

---
+++ ArrayforEach.js
@@ -0,0 +1,19 @@
+alert
+
+
+const Num=[45, 4, 9, 16, 25];
+Num.forEach(myFunction);
+function myFunction(value,index,array){
+  let T3="";
+  T3 += value;
+  console.log("your T3 is:" +T3);
+} 
+
+let sum= 0;
+let cart= [5,6,7,8,9];
+function checkout(element, index, array){
+  sum +=element;
+}
+cart.forEach(checkout);
+console.log("your sum is: $" +sum);
+
