689d581c9226b77f68d1d6128b0089f5804ab376
tiwavaldese <tiwaval7@gmail.com>
2023-01-18 09:13:33 +0100

    forofLoop.js

---
+++ forofLoop.js
@@ -0,0 +1,25 @@
+alert
+
+
+const Cars= ['BMW', 'volvo', 'Mini'];
+let T4= "";
+for(let x of Cars){
+  
+  T4 += x;
+  console.log(x);
+}
+
+const language= "Javascript";
+let T5= "";
+for(let x of language){
+  T5 += x;
+  console.log(x);
+}
+
+let nums= [];
+nums[0]= 5;
+nums[99]= 9;
+for(let n of nums){
+  console.log(n);
+}
+  
