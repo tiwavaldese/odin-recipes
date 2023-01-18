075f64666c8b9269ac144c65ed94f9563df7514d
tiwavaldese <tiwaval7@gmail.com>
2023-01-18 11:18:11 +0100

    forofLoop.js

--- forofLoop.js
+++ forofLoop.js
@@ -23,3 +23,26 @@ for(let n of nums){
   console.log(n);
 }
   
+
+const namE=('W3Schools');
+for(const x of namE){
+  console.log(x);
+}
+
+const letterS=['A','B','C'];
+for(const x of letterS){
+  console.log(x);
+}
+
+const letteRs=new Set(['a','b','c']);
+for(const x of letteRs){
+  console.log(x);
+}
+const Fruits = new Map([
+  ['Apples',500],
+  ['Bananas',300],
+  ['Oranges',200],
+])
+for(const x of Fruits){
+  console.log(x);
+}
