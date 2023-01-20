f9d9e3a09381c7559b3e559d2a1852f5f48c8c92
tiwavaldese <tiwaval7@gmail.com>
2023-01-20 18:52:05 +0100

    function.js

--- function.js
+++ function.js
@@ -13,7 +13,8 @@ greet('son');
 
 function add(x,y){
    console.log(x + y);
-}add(9,7);
+}
+add(9,7);
 
 let sUm = function add(x,y){
     console.log(x + y);
@@ -22,3 +23,16 @@ let sUm = function add(x,y){
  let suM = function (x,y){
     console.log(x + y);
  } (100,15)
+
+ const multiply= (x,y)=>{
+    console.log(x*y);
+ }
+ multiply(40,4);
+
+ const square= x =>{
+    console.log(x*x);
+ }
+ square(9);
+
+ const squares = x =>console.log(x*x);
+squares(12);
